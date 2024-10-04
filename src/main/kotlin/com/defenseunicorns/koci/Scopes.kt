2fec9521c9066e04767d4bab3e601540cb6ef525
razzle <harry@razzle.cloud>
2024-10-03 21:49:23 -0400

    not working, but getting there
    Signed-off-by: razzle <harry@razzle.cloud>

--- src/main/kotlin/com/defenseunicorns/koci/Scopes.kt
+++ src/main/kotlin/com/defenseunicorns/koci/Scopes.kt
@@ -32,14 +32,102 @@ const val ACTION_DELETE = "delete"
 // ACTION_REGISTRY_CATALOG is the scope for registry catalog access.
 const val ACTION_REGISTRY_CATALOG = "registry:catalog:*"
 
-fun scopeRepository(repo: String, scopes: List<String>): String {
-    val actions = cleanActions(scopes)
+fun scopeRepository(repo: String, actions: List<String>): String {
+    val cleaned = cleanActions(actions)
 
-    return listOf("repository", repo, actions.joinToString(",")).joinToString(":")
+    return listOf("repository", repo, cleaned.joinToString(",")).joinToString(":")
 }
 
-private fun cleanActions(actions: List<String>): List<String> {
-    return actions.map { it.trim() }.filter { it.isNotEmpty() }.distinct()
+fun cleanActions(scopes: List<String>): List<String> {
+    return scopes.map { it.trim() }.filter { it.isNotEmpty() }.distinct()
+}
+
+fun cleanScopes(scopes: List<String>): List<String> {
+    // fast paths
+    when (scopes.size) {
+        0 -> return emptyList()
+        1 -> {
+            val scope = scopes[0]
+            val i = scope.lastIndexOf(":")
+            if (i == -1) {
+                return listOf(scope)
+            }
+            var actionList = scope.substring(i + 1).split(",")
+            actionList = cleanActions(actionList)
+            if (actionList.isEmpty()) {
+                return emptyList()
+            }
+
+            val actions = actionList.joinToString(",")
+            return listOf(
+                scope.substring(0, i + 1) + actions
+            )
+        }
+    }
+
+    // slow path
+    val list = mutableListOf<String>()
+
+    val resourceTypes = mutableMapOf<String, MutableMap<String, MutableSet<String>>>()
+
+    for (scope in scopes) {
+        val i = scope.indexOf(":")
+        if (i == -1) {
+            list.add(scope)
+            continue
+        }
+
+        val resourceType = scope.substring(0, i)
+
+        // extract resource name and actions
+        val rest = scope.substring(i + 1)
+        val actionsDivider = rest.lastIndexOf(":")
+        if (actionsDivider == -1) {
+            list.add(scope)
+            continue
+        }
+        val actions = rest.substring(actionsDivider + 1)
+        if (actions.isEmpty()) {
+            // drop since no actions found
+            continue
+        }
+        val resourceName = rest.substring(0, actionsDivider)
+
+        val namedActions = resourceTypes.getOrDefault(resourceType, mutableMapOf())
+
+        for (action in actions.split(",")) {
+            if (action.isNotEmpty()) {
+                namedActions[resourceName]?.add(action)
+
+                println("adding $action to $resourceName")
+            }
+        }
+    }
+
+    for ((resourceType, namedActions) in resourceTypes) {
+        for ((resourceName, actionSet) in namedActions) {
+            if (actionSet.isEmpty()) {
+                continue
+            }
+
+            var actions = mutableListOf<String>()
+            for (action in actionSet) {
+                if (action == "*") {
+                    actions = mutableListOf("*")
+                    break
+                }
+                actions.add(action)
+            }
+            actions.sort()
+
+            val scope = "$resourceType:$resourceName:${actions.joinToString(",")}"
+            list.add(scope)
+        }
+    }
+
+    list.sort()
+
+    return list
 }
 
 private val scopesKey = AttributeKey<String>("scopesKey")
@@ -53,11 +141,14 @@ val ScopesPlugin = createClientPlugin("ScopesPlugin") {
 
                 val realm = authHeader.parameters.first { it.name == "realm" }.value
                 val service = authHeader.parameters.first { it.name == "service" }.value
-                val scope = authHeader.parameters.first { it.name == "scope" }.value
+                val challengeScope = authHeader.parameters.first { it.name == "scope" }.value
+
+                val requestScopes = request.attributes[scopesKey]
+
 
                 // do auth flow
                 val authURL = URLBuilder().takeFrom(realm).apply {
-                    parameters.append("scope", scope)
+                    parameters.append("scope", challengeScope)
                     parameters.append("service", service)
                 }.build()
 
---
+++ src/test/kotlin/com/defenseunicorns/koci/ScopesTest.kt
@@ -0,0 +1,59 @@
+package com.defenseunicorns.koci
+
+import kotlin.test.Test
+import kotlin.test.assertEquals
+import kotlin.test.assertFails
+
+class ScopesTest {
+    @Test
+    fun valid() {
+        val testCases = listOf(
+            listOf("") to listOf(""),
+            listOf(ACTION_REGISTRY_CATALOG) to listOf(ACTION_REGISTRY_CATALOG),
+            listOf(
+                scopeRepository(
+                    "ubuntu",
+                    listOf(ACTION_PULL, ACTION_PUSH)
+                )
+            ) to listOf(
+                scopeRepository(
+                    "ubuntu",
+                    listOf(ACTION_PULL, ACTION_PUSH)
+                )
+            ),
+            listOf(
+                scopeRepository(
+                    "ubuntu",
+                    listOf(ACTION_PULL)
+                ),
+                scopeRepository(
+                    "ubuntu",
+                    listOf(ACTION_PUSH)
+                )
+            ) to listOf(
+                scopeRepository(
+                    "ubuntu",
+                    listOf(ACTION_PULL, ACTION_PUSH)
+                )
+            )
+        )
+
+        for ((dirty, clean) in testCases) {
+            val actual = cleanScopes(dirty)
+            assertEquals(clean, actual)
+        }
+    }
+
+    @Test
+    fun invalid() {
+        val testCases = listOf(
+            listOf("") to ""
+        )
+
+        for ((dirty, message) in testCases) {
+            assertFails(message) {
+                cleanScopes(dirty)
+            }
+        }
+    }
+}

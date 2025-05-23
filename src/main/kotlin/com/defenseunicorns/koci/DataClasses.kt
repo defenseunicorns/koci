714604ef21986aa343a0df7c725635f45546d668
Harry Randazzo <harry@razzle.cloud>
2025-05-23 01:36:48 -0500

    descriptor
    Signed-off-by: Harry Randazzo <harry@razzle.cloud>

--- src/main/kotlin/com/defenseunicorns/koci/DataClasses.kt
+++ src/main/kotlin/com/defenseunicorns/koci/DataClasses.kt
@@ -155,14 +155,45 @@ data class Platform(
     val variant: String? = null,
 )
 
+/**
+ * Descriptor describes the disposition of targeted content.
+ *
+ * This structure provides `application/vnd.oci.descriptor.v1+json` mediatype
+ * when marshalled to JSON.
+ */
 @Serializable
 data class Descriptor(
+    /**
+     * mediaType is the media type of the object this schema refers to.
+     */
     val mediaType: String,
+    /**
+     * digest is the digest of the targeted content.
+     */
     val digest: Digest,
+    /**
+     * size specifies the size in bytes of the blob.
+     */
     val size: Long,
+    /**
+     * urls specifies a list of URLs from which this object MAY be downloaded
+     */
     val urls: List<String>? = null,
+    /**
+     * annotations contains arbitrary metadata relating to the targeted content.
+     */
     val annotations: Annotations? = null,
+    /**
+     * data is an embedding of the targeted content. This is encoded as a base64
+     * string when marshalled to JSON (automatically, by encoding/json). If
+     * present, data can be used directly to avoid fetching the targeted content.
+     */
     val data: String? = null,
+    /**
+     * platform describes the platform which the image in the manifest runs on.
+     *
+     * This should only be used when referring to a manifest.
+     */
     val platform: Platform? = null,
 ) {
     companion object {

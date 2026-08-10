7439f6f0e9ff8b218bd533cd41923859ad6d6cb8
Landon Patmore <landon@defenseunicorns.com>
2026-08-10 13:34:39 -0400

    chore: dependabot ignores koci and now use library toml (#179)
    Dependabot was trying to update koci, but it is purposefully older so we
    can test against the latest and oras. Also moves deps to fully use the
    versions toml.

--- bench/harness/build.gradle.kts
+++ bench/harness/build.gradle.kts
@@ -1,10 +1,10 @@
 plugins {
-  kotlin("jvm") version "2.2.10"
-  kotlin("plugin.serialization") version "2.2.10"
+  alias(libs.plugins.jetbrains.kotlin.jvm)
+  alias(libs.plugins.kotlin.serialization)
 }
 
 kotlin { jvmToolchain(21) }
 
 dependencies {
-  implementation("org.jetbrains.kotlinx:kotlinx-serialization-json:1.9.0")
+  implementation(libs.kotlinx.serialization.json)
 }
